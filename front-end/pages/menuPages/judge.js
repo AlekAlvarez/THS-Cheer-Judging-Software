f77225be93cfe2f55aae193801293f350db7c93c
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-05-11 22:46:02 -0500

    Completes form

--- front-end/pages/menuPages/judge.js
+++ front-end/pages/menuPages/judge.js
@@ -61,8 +61,9 @@ addStudent.addEventListener("click",()=>{
             }
             form.appendChild(ToeTouchLabel); form.appendChild(ToeTouchSelect);
             const secondJumpLabel=document.createElement("label");
-            secondJumpLabel.setAttribute("for","2nd-touch"); secondJumpLabel.innerText="2nd Jump:";
+            secondJumpLabel.setAttribute("for","2nd-jump"); secondJumpLabel.innerText="2nd Jump:";
             form.appendChild(secondJumpLabel);const secondJumpSelect=document.createElement("select");
+            secondJumpSelect.setAttribute('name',"2nd-jump");
             for(let i=1;i<=5;i++){
                 let option=document.createElement("option");option.setAttribute("value",""+i);
                 option.innerText=i; secondJumpSelect.appendChild(option);
@@ -71,11 +72,100 @@ addStudent.addEventListener("click",()=>{
             const overallGameDayAppealJumpLabel=document.createElement("label");
             overallGameDayAppealJumpLabel.setAttribute("for","jumpAppeal"); overallGameDayAppealJumpLabel.innerText="Overrall Game Day Jump Appeal: ";
             form.appendChild(overallGameDayAppealJumpLabel);const jumpAppealSelect=document.createElement("select");
+            jumpAppealSelect.setAttribute("name","jumpAppeal");
             for(let i=1;i<=10;i++){
                 let option=document.createElement("option");option.setAttribute("value",""+i);
                 option.innerText=i; jumpAppealSelect.appendChild(option);
             }
             form.appendChild(jumpAppealSelect);
+            const BandChantLegends=document.createElement("legend");
+            BandChantLegends.innerText="Band Chant:";
+            form.appendChild(BandChantLegends);
+            const bandMotionLabel=document.createElement("label");
+            bandMotionLabel.setAttribute("for","bandMotion");bandMotionLabel.innerText="Motions (Technique & Sharpness)";
+            form.appendChild(bandMotionLabel);const bandAppealSelect=document.createElement("select");
+            bandAppealSelect.setAttribute("name","bandMotion");
+            for(let i=1;i<=10;i++){
+                let option=document.createElement("option"); option.setAttribute("value",""+i);
+                option.innerText=i;bandAppealSelect.appendChild(option);
+            }
+            form.appendChild(bandAppealSelect);
+            const bandCadenceLabel=document.createElement("label");
+            bandCadenceLabel.innerText="Knowledge of Cadence";bandCadenceLabel.setAttribute("for","band-cadence");
+            form.appendChild(bandCadenceLabel); const bandCadenceSelect=document.createElement("select");bandCadenceSelect.setAttribute("name","band-cadence");
+            bandCadenceSelect.setAttribute("name","band-cadence");
+            addOptions(5,bandCadenceSelect); form.appendChild(bandCadenceSelect);
+            const bandEnergyLabel=document.createElement("label");
+            bandEnergyLabel.innerText="Showmanship/Energy";bandEnergyLabel.setAttribute("for","band-energy");
+            form.appendChild(bandEnergyLabel);const bandEnergySelect=document.createElement("select");
+            bandEnergySelect.setAttribute("name","band-energy");
+            addOptions(10,bandEnergySelect);form.appendChild(bandEnergySelect);
+            const rhythm=document.createElement("label"); rhythm.innerText="Rhythm";
+            rhythm.setAttribute("for","rhythm"); form.appendChild(rhythm);
+            const rhythmSelect=document.createElement("select"); rhythmSelect.setAttribute("name","rhythm");
+            addOptions(10,rhythmSelect);form.appendChild(rhythmSelect);
+            const bandClarity=document.createElement("label");bandClarity.innerText="Voice Projection & Clarity:";
+            bandClarity.setAttribute("for","band-clarity");form.appendChild(bandClarity);
+            const bandClaritySelect=document.createElement("select");bandClaritySelect.setAttribute("name","band-clarity");
+            addOptions(5,bandClaritySelect);form.appendChild(bandClaritySelect);
+            const bandAppeal=document.createElement("label");bandAppeal.innerText="Overall Game Day Appeal:";
+            bandAppeal.setAttribute("for","band-appeal");form.appendChild(bandAppeal);
+            const bandASelect=document.createElement("select");bandASelect.setAttribute("name","band-appeal");
+            addOptions(10,bandASelect);form.appendChild(bandASelect);
+            const sidelineLegends=document.createElement("legend"); 
+            sidelineLegends.innerText="Situational Sideline"; form.appendChild(sidelineLegends);
+            const situationalMotion=document.createElement("label");situationalMotion.innerText="Motions (Technique & Sharpness)";
+            situationalMotion.setAttribute("for","situational-motion");form.appendChild(situationalMotion);
+            const situationalMotionSelect=document.createElement("select");situationalMotionSelect.setAttribute("name","situational-motion");
+            addOptions(5,situationalMotionSelect);form.appendChild(situationalMotionSelect);
+            const sideChant=document.createElement("label");sideChant.innerText="Knowledge of Chant";
+            sideChant.setAttribute("for","side-chant");form.appendChild(sideChant);
+            const sideChantSelect=document.createElement("select"); sideChantSelect.setAttribute("name","side-chant");
+            addOptions(5,sideChantSelect);form.appendChild(sideChantSelect);
+            const sideEnergy=document.createElement("label");sideEnergy.innerText="Showmanship/Energy";
+            sideEnergy.setAttribute("for","side-energy");form.appendChild(sideEnergy);
+            const sideEnergySelect=document.createElement("select");sideEnergySelect.setAttribute("name","side-energy");
+            addOptions(5,sideEnergySelect);form.appendChild(sideEnergySelect);
+            const sideClarity=document.createElement("label");sideClarity.innerText="Voice Projection & Clarity";
+            sideClarity.setAttribute("for","side-clarity");form.appendChild(sideClarity);
+            const sideClaritySelect=document.createElement("select");sideClaritySelect.setAttribute("name","side-clarity");
+            addOptions(5,sideClaritySelect);form.appendChild(sideClaritySelect);
+            const sideAppeal=document.createElement("label");sideAppeal.innerText="Overall Game Day Appeal";
+            sideAppeal.setAttribute("for","side-appeal");form.appendChild(sideAppeal);
+            const sideAppealSelect=document.createElement("select");sideAppealSelect.setAttribute("name","side-appeal");
+            addOptions(10,sideAppealSelect);form.appendChild(sideAppealSelect);
+            const cheerLegends=document.createElement("legend");cheerLegends.innerText="Cheer:";
+            form.appendChild(cheerLegends);
+            const cheerMotion=document.createElement("label");cheerMotion.innerText="Motions (Technique & Sharpness)";
+            cheerMotion.setAttribute("for","cheer-motion");form.appendChild(cheerMotion);
+            const cheerMotionSelect=document.createElement("select");cheerMotionSelect.setAttribute("name","cheer-motion");
+            addOptions(10,cheerMotionSelect);form.appendChild(cheerMotionSelect);
+            const cheerKnowledge=document.createElement("label");cheerKnowledge.innerText="Knowledge of Cheer";
+            cheerKnowledge.setAttribute("for","cheer-knowledge");form.appendChild(cheerKnowledge);
+            const cheerKnowledgeSelect=document.createElement("select");cheerKnowledgeSelect.setAttribute("name","cheer-knowledge");
+            addOptions(5,cheerKnowledgeSelect);form.appendChild(cheerKnowledgeSelect);
+            const cheerEnergy=document.createElement("label");cheerEnergy.innerText="Showmanship/Energy";
+            cheerEnergy.setAttribute("for","cheer-energy");form.appendChild(cheerEnergy);
+            const cheerEnergySelect=document.createElement("select");cheerEnergySelect.setAttribute("name","cheer-energy");
+            addOptions(10,cheerEnergySelect);form.appendChild(cheerEnergySelect);
+            const cheerClarity=document.createElement("label");cheerClarity.innerText="Voice Projection & Clarity";
+            cheerClarity.setAttribute("for","cheer-clarity");form.appendChild(cheerClarity);
+            const cheerClaritySelect=document.createElement("select");cheerClaritySelect.setAttribute("name","cheer-clarity");
+            addOptions(5,cheerClaritySelect);form.append(cheerClaritySelect);
+            const cheerExecution=document.createElement("label");cheerExecution.innerText="Skill Execution Jump (1-3) BHS(1-10)";
+            cheerExecution.setAttribute("for","cheer-execution");form.appendChild(cheerExecution);
+            const cheerExecutionSelect=document.createElement("select");cheerExecutionSelect.setAttribute("name","cheer-execution");
+            addOptions(10,cheerExecutionSelect);form.append(cheerExecutionSelect);
+            const cheerAppeal=document.createElement("label");cheerAppeal.innerText="Overall Game Day Appeal";
+            cheerAppeal.setAttribute("for","cheer-appeal");form.appendChild(cheerAppeal);
+            const cheerAppealSelect=document.createElement("select");cheerAppealSelect.setAttribute("name","cheer-aappeal");
+            addOptions(10,cheerAppealSelect);form.appendChild(cheerAppealSelect);
+            const backLegend=document.createElement("legend");
+            backLegend.innerText="Back HandSpring:";form.appendChild(backLegend);
+            const backLabel=document.createElement("label");backLabel.innerText="Back Handspring";
+            backLabel.setAttribute("for","back-handspring");form.appendChild(backLabel);
+            const backCheck=document.createElement("input");
+            backCheck.setAttribute("type","checkbox");form.appendChild(backCheck);
             studentMap.set(studentNumber, form);
             switchForm(studentNumber);
             newStudent.addEventListener("click",()=>{
@@ -90,4 +180,10 @@ function switchForm(num){
         judgingForm.removeChild(oldForm[i]);
     }
     judgingForm.appendChild(studentMap.get(num));
+}
+function addOptions(optionNumbers,parentNode){
+    for(let i=1;i<=optionNumbers;i++){
+        let option=document.createElement("option"); option.setAttribute("value",""+i);
+        option.innerText=i;parentNode.appendChild(option);       
+    }
 }
